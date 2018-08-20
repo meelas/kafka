a4383aafa796e6c08ae41eb1ae2a3de296de3e12
Mykhailo Mychkivskyi <mykhailo_mychkivskyi@epam.com>
2018-08-20 18:33:05 +0300

    add send interval for consumer

--- producer/src/main/java/com/pru/ProducerApplication.java
+++ producer/src/main/java/com/pru/ProducerApplication.java
@@ -1,14 +1,36 @@
 package com.pru;
 
 
+import com.pru.domain.Job;
+import com.pru.produce.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.concurrent.TimeUnit;
+
 @EnableEurekaClient
 @SpringBootApplication
-public class ProducerApplication  {
+public class ProducerApplication implements CommandLineRunner {
+    @Autowired
+    private Producer producer;
+
+    @Value("${app.send.interval}")
+    private int interval;
+
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        while (true){
+            Job job = new Job();
+            producer.send(job);
+            TimeUnit.SECONDS.sleep(interval);
+        }
     }
 }
