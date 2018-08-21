9832b5f80d24850037cf9170b672e22af4d17adc
Mykhailo Mychkivskyi <mykhailo_mychkivskyi@epam.com>
2018-08-21 12:35:32 +0300

    fix configuration

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,12 @@ RUN apt-get install -y maven
 
 RUN ls -l
 
-CMD ["echo", "Start"]
+WORKDIR /code
+
+ADD pom.xml /code/pom.xml
+ADD config /code/config
+ADD consumer /code/consumer
+ADD eureka /code/eureka
+ADD producer /code/producer
+
+CMD ["mvn", "install"]
--- consumer/src/main/java/com/pru/KafkaConsumerConfig.java
+++ consumer/src/main/java/com/pru/KafkaConsumerConfig.java
@@ -4,7 +4,6 @@ import com.pru.domain.JobDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
